05e0ac0797b6c4bcd96952cc883f5153ae984f80
user <venkatinuk@gmail.com>
2021-12-18 13:40:05 +0000

    commit

---
+++ src/main/java/uk/axone/devintest/git1/Application.java
@@ -0,0 +1,10 @@
+package uk.axone.devintest.git1;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("git is cool");
+
+    }
+
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class AppTest {
+
+   @Test
+    public void test1(){
+        System.out.println("runnung test1");
+            }
+    @Test
+    public void test2(){
+        System.out.println("runnung test2");
+            }
+    @Test
+    public void test3(){
+        System.out.println("runnung test3");
+    }
+
+
+}
